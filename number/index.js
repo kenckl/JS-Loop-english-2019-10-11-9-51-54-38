2299667b7ff75a7ea5fa7d716d7a09834f09d08c
kenckl <44955521+kenckl@users.noreply.github.com>
2020-11-18 14:23:48 +0800

    Update index.js

--- number/index.js
+++ number/index.js
@@ -1,9 +1,9 @@
 for(let i = 1; i <= 20; i++) {
 
     if (i % 2 == 0)
-    	document.write(i + " is an even number.");
+    	document.write(i + " is even number.");
     else
-    	document.write(i + " is an odd number.");
+    	document.write(i + " is odd number.");
     
     document.write("<br>");
 }
