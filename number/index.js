faae1fbe0a90b7a285e26831a711a03f1ecd28ce
kenckl <44955521+kenckl@users.noreply.github.com>
2020-11-18 14:23:12 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for(let i = 1; i <= 20; i++) {
+
+    if (i % 2 == 0)
+    	document.write(i + " is an even number.");
+    else
+    	document.write(i + " is an odd number.");
+    
+    document.write("<br>");
+}
